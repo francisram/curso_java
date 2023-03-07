de9dc356bb6db26413084950948142c35e0e48d1
francisram <38248947+francisram@users.noreply.github.com>
2023-03-07 16:25:43 -0300

    botones

--- test/src/py/edu/ucsa/test/clases/ManejadorRecursos.java
+++ test/src/py/edu/ucsa/test/clases/ManejadorRecursos.java
@@ -1,11 +1,18 @@
 package py.edu.ucsa.test.clases;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ManejadorRecursos {
 	private List<Recurso> recursos;
 
 	public List<Recurso> getRecursos() {
+		
+		if(Objects.isNull(recursos)) {
+			recursos = new ArrayList<>();
+		}
+		
 		return recursos;
 	}
  
--- test/src/py/edu/ucsa/test/clases/Principal.java
+++ test/src/py/edu/ucsa/test/clases/Principal.java
@@ -5,38 +5,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		FileReader reader;
+		FileReader readerb;
+		FileReader readerp;
 		try {
-			reader = new FileReader("c:/java/io-test.txt");
-			BufferedReader bufReader=new BufferedReader(reader);
-
+			reader = new FileReader("C:\\tarea\\Menus.txt");
+			readerb = new FileReader("C:\\tarea\\Botones.txt");
+			readerp = new FileReader("C:\\\\tarea\\\\Paginas.txt");
+			BufferedReader bufReader = new BufferedReader(reader);
+			BufferedReader bufReaderboton = new BufferedReader(readerb);
+			BufferedReader bufReaderpag = new BufferedReader(readerp);
 			String line = null;
-
+			String lineb = null;
+			String linep = null;
 			line = bufReader.readLine();
+			ManejadorRecursos mr = new ManejadorRecursos();
+			while (line != null) {
+				String[] menus = line.split(",");
+				Menu m = new Menu();
+				m.setId(Integer.parseInt(menus[0]));
+				m.setAbreviatura(menus[1]);
+				m.setDescripcion(menus[2]);
+				boolean p = (menus[3] == "s") ? true : false;
+				m.setPadre(p);
+				mr.getRecursos().add(m);
 
-			while(line != null){
-
-			System.out.println(line);
-
-			line = bufReader.readLine();
-
+				line = bufReader.readLine();
 			}
-
-			//while((line = bufReader.readLine()) != null)
-
-			//System.out.println(line);
-
+			mr.getRecursos();
+			lineb = bufReaderboton.readLine();
+			while (lineb !=null ) {
+				System.out.println(lineb);
+				lineb = bufReaderboton.readLine();
+				
+			}
+			
+			
 			bufReader.close();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-	
-		
 	}
 }
